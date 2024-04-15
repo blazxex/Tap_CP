f3350a1c58caaf2f30a9a2f6fe4ddd9abe9528ea
Blazxex <armygm162@gmail.com>
2024-04-16 00:30:25 +0800

    implement getCurrentBoss func

--- backend/src/routes/bossRoute.js
+++ backend/src/routes/bossRoute.js
@@ -5,5 +5,6 @@ import * as bossControl from "../controllers/bossController.js";
 const router = express.Router();
 
 router.get("/", bossControl.createBoss);
+router.get("/current", bossControl.getCurrentBoss);
 
 export default router;
