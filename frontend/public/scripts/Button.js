c84fa8e207e3086f4b5e7982432fcee4debb4186
paopeaw <paopeaw8246@gmail.com>
2024-04-17 22:37:33 +0700

    Add element icon

--- frontend/public/scripts/Button.js
+++ frontend/public/scripts/Button.js
@@ -5,18 +5,18 @@ export default class Button extends Phaser.GameObjects.Container{
     constructor(scene, x,y,scale,image,name,item){
         super(scene,x,y);
         this.name = name;
-        let img = scene.add.image(0,0,image).setScale(scale);
+        this.img = scene.add.image(0,0,image).setScale(scale);
         // level and dmage text.
        
         this.item = item;
-        this.lvTxt = scene.add.text(-70,-120, `lv: ${item[0].itemLevel}`).setFontSize(16).setColor('#ffff00');
-        this.dmgTxt = scene.add.text(0,-120, `dmg: ${item[0].attackPower}`).setFontSize(16).setColor('#ffff00');
+        this.lvTxt = scene.add.text(-70,-100, `lv: ${item[0].itemLevel}`).setFontSize(16).setColor('#ffff00');
+        this.dmgTxt = scene.add.text(0,-100, `dmg: ${item[0].attackPower}`).setFontSize(16).setColor('#ffff00');
 
         // upgrade btn
-        this.upgradeTxt = scene.add.text(-50,120, `lv:${item[0].itemLevel+1}  5pt.`).setFontSize(16).setColor('#000000');
-        this.upgradeBtn = scene.add.rectangle(0, 130, img.width*scale, img.height*scale*.1, 0xffffff);
+        this.upgradeTxt = scene.add.text(-50,100, `lv:${item[0].itemLevel+1}  5pt.`).setFontSize(16).setColor('#000000');
+        this.upgradeBtn = scene.add.rectangle(0, 110, this.img.width*scale, 80*scale, 0xffffff);
 
-        this.add(img);
+        this.add(this.img);
         this.add(this.lvTxt);
         this.add(this.dmgTxt);
         this.add(this.upgradeBtn)
@@ -41,18 +41,18 @@ export default class Button extends Phaser.GameObjects.Container{
 
 
 
-        this.whiteFill = scene.add.rectangle(0, 0, img.width*scale, img.height*scale, 0xffffff);
+        this.whiteFill = scene.add.rectangle(0, 0, this.img.width*scale, this.img.height*scale, 0x000000);
         this.whiteFill.setAlpha(0); // Initially invisible
         this.add(this.whiteFill);
-        this.setSize(img.width*scale, img.height*scale);
+        this.setSize(this.img.width*scale, this.img.height*scale);
         this.setInteractive()
             .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OVER,()=>{
                 // console.log('hover')
-                img.setAlpha(0.5)//set image opacity to 0.5
+                this.img.setAlpha(0.5)//set image opacity to 0.5
             })
             .on(Phaser.Input.Events.GAMEOBJECT_POINTER_OUT,()=>{
                 // console.log('out')
-                img.setAlpha(1)//set image opacity to 1
+                this.img.setAlpha(1)//set image opacity to 1
             })
     }
 
