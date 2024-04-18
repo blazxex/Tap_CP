87d6268e3273814a94604c1795625f620029ee4b
Blazxex <armygm162@gmail.com>
2024-04-18 23:57:04 +0700

    implement score update

--- backend/src/controllers/scoreBoardController.js
+++ backend/src/controllers/scoreBoardController.js
@@ -32,3 +32,37 @@ export const getScore = async (req, res) => {
     res.status(500).json({ message: "Error retrieving user", error: error });
   }
 };
+
+export const updateScore = async (req, res) => {
+  const { userCookieId, score } = req.body;
+
+  // Validate that both userCookieId and score are provided
+  if (!userCookieId || score === undefined) {
+    return res.status(400).json({ message: "Missing userCookieId or score" });
+  }
+
+  // Optionally, validate that score is a number, not a string or other type
+  if (typeof score !== "number") {
+    return res.status(400).json({ message: "Invalid score type" });
+  }
+
+  try {
+    const updatedScore = await ScoreBoard.findOneAndUpdate(
+      { userCookieId: userCookieId }, // Adjusted based on the actual type of userCookieId
+      { $set: { score: score } },
+      { new: true }
+    );
+
+    if (updatedScore) {
+      res
+        .status(200)
+        .json({ message: "Score updated successfully", data: updatedScore });
+    } else {
+      res.status(404).json({ message: "User not found" });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error updating score", error: error.message });
+  }
+};
--- backend/src/routes/scoreBoardRoute.js
+++ backend/src/routes/scoreBoardRoute.js
@@ -6,5 +6,6 @@ const router = express.Router();
 
 router.get("/", scoreBoardController.getScoreBoard);
 router.get("/user", scoreBoardController.getScore);
+router.post("/update", scoreBoardController.updateScore);
 
 export default router;
