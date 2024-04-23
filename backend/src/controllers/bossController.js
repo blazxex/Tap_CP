30a7e605d116c4993a9aaca81a5c97fc395c8a42
paopeaw <paopeaw8246@gmail.com>
2024-04-23 21:04:48 +0700

    change boss health

--- backend/src/controllers/bossController.js
+++ backend/src/controllers/bossController.js
@@ -11,7 +11,7 @@ function getRandomBossName() {
 }
 
 function getRandomHealth() {
-  return (Math.floor(Math.random() * 1000) + 50) * 10;
+  return ((Math.floor(Math.random() * 1000) + 50)*10) + 10000;
 }
 function getRandomWeakness(){//0,1,2
   return (Math.floor(Math.random() * 3))
